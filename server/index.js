80126eed24263499fb9144236d068276c2375d66
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-10-09 16:58:30 +0100

    refactor: prettied index.js and routes.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const router = require("./routes");
+const express = require('express');
+const router = require('./routes');
 
 const app = express();
 
 const PORT = 3000;
 
-app.use("/", router);
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Server Listening 👂 on at http://localhost:${PORT}`);
--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.status(200);
-  res.send("Hello World!");
+  res.send('Hello World!');
 });
 
 module.exports = router;
