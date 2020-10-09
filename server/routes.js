18fb7b9ed62f7822c9292bf204798a1647e61571
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-10-09 18:05:22 +0100

    refactor: converted routes.js -> routes.ts

--- server/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.status(200);
-  res.send('Hello World!');
-});
-
-module.exports = router;
