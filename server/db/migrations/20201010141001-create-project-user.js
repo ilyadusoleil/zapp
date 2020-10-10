31f824bf9edd44635580b42f732694de8b0d0243
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-10-10 15:32:44 +0100

    feat: generated project user schema

---
+++ server/db/migrations/20201010141001-create-project-user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ProjectUsers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      projectId: {
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      authorization: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ProjectUsers');
+  }
+};
---
+++ server/db/models/projectuser.js
@@ -0,0 +1,45 @@
+'use strict';
+const { Model } = require('sequelize');
+const project = require('./project');
+const user = require('./user');
+module.exports = (sequelize, DataTypes) => {
+  class ProjectUser extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ProjectUser.init(
+    {
+      projectId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: project,
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: user,
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      authorization: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'ProjectUser',
+    }
+  );
+  return ProjectUser;
+};
