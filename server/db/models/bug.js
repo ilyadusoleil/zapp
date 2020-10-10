9a9e762c9f780dfa63b4df2de8870aa1898ae45b
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-10-10 09:56:54 +0100

    feat: generated bug model

---
+++ server/db/models/bug.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class bug extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  bug.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    state: DataTypes.STRING,
+    priority: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'bug',
+  });
+  return bug;
+};
