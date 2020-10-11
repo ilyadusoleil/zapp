9c9bf5c12cf985683657815db2bd4787c2b4e14f
Rupert Bright <rupert.bright@live.co.uk>
2020-10-11 19:12:00 +0100

    feat: card-styling

--- client/src/components/Bugitem.tsx
+++ client/src/components/Bugitem.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Collapse } from 'react-collapse';
 import { Bug } from '../types/Bug';
+import './Bugitem.css';
 
 function Bugitem({ bug }: { bug: Bug }) {
   const [isOpened, setisOpened] = useState(false);
@@ -14,24 +15,27 @@ function Bugitem({ bug }: { bug: Bug }) {
   }
 
   return (
-    <div className=" rounded-sm m-2 bg-gray-100 w-1/3 border-2 border-indigo-100 ">
+    <div className=" m-2 bg-gray-100 w-2/5 border-2 border-indigo-200 ">
       <div
         onClick={() => setisOpened(!isOpened)}
-        className="bg-gray-100 p-1 flex justify-items"
+        className="bg-gray-100 ml-2 p-1 flex justify-items cursor-pointer"
       >
         <h1>{bug.title}</h1>
         <p
           className={`ml-auto ${priority(
             bug.priority
-          )} rounded-lg p-1 text-xs w-16 justify-self-center`}
+          )} rounded-full text-xs w-16 pt-1 flex justify-center text-gray-200`}
         >
           {bug.priority}
         </p>
       </div>
 
-      <Collapse className="" isOpened={isOpened}>
-        <div className="p-2 divide-y divide-gray-400 duration-300">
-          <p>{bug.id}</p>
+      <Collapse  isOpened={isOpened}>
+        <div className="p-5 divide-y divide-gray-400 text-gray-800">
+            <div className='flex'>
+                <p>{bug.id}</p>
+                <p className='ml-auto'>Due date: 12/10/2020</p>
+          </div>
           <p className="mt-2 py-6">{bug.description}</p>
         </div>
       </Collapse>
