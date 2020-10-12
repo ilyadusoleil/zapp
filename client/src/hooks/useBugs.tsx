369bd1908c94a30be9a932131aa5ecc55169290f
Hamish <chan.hamish@gmail.com>
2020-10-12 12:12:24 +0100

    feat: extend mock db endpoints

--- client/src/hooks/useBugs.tsx
+++ client/src/hooks/useBugs.tsx
@@ -2,8 +2,9 @@ import { useQuery, QueryResult } from 'react-query';
 import fetchRequest from '../services/ApiService';
 import { Bug } from '../types/Bug';
 
-const useBugs = (): QueryResult<Bug[]> => {
-  return useQuery('project', () => fetchRequest('/project'));
+// Get all bugs for one project
+const useBugs = (projectId: string): QueryResult<Bug[]> => {
+  return useQuery(projectId && ['projectbugs', projectId], () => fetchRequest(`/bugs?projectId=${projectId}`));
 }
 
 export default useBugs
