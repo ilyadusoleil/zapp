5d531e98ff9c28bb0c0483d78d7da4c834ecedf1
Rupert Bright <rupert.bright@live.co.uk>
2020-10-12 19:35:22 +0100

    feat: form modal added

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link, navigate, RouteComponentProps } from '@reach/router';
+import Modal from 'react-modal';
 
 import { Bug } from '../types/Bug';
 import useBugs from '../hooks/useBugs';
 
 import Bugitem from '../components/Bugitem';
 import Sidebar from '../components/Sidebar';
+import BugEditForm from '../components/BugEditForm';
 
 const Dashboard = (_props: RouteComponentProps) => {
   const { isLoading, isError, data } = useBugs();
+  const [modalIsOpen, setModalIsOpen] = useState(false)
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -18,14 +21,30 @@ const Dashboard = (_props: RouteComponentProps) => {
     return <span>Error: </span>;
   }
 
+  const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)'
+    }
+  };
+
+
   return (
     <>
+    
       <Sidebar />
+      <Modal isOpen={modalIsOpen} style={customStyles}>
+          <BugEditForm setModalIsOpen={setModalIsOpen}/>
+      </Modal >
       <div className="mx-16">
-        <h1>Dashboard</h1>
-
+        <h1>Dashboard</h1
+>
         {data.map((bug: Bug, index) => (
-          <Bugitem key={index} bug={bug} />
+          <Bugitem setModalIsOpen={setModalIsOpen} key={index} bug={bug} />
         ))}
       </div>
     </>
