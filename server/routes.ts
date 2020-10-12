e7ebadbad4337fab87f292fc8cb334a48202bb78
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-10-12 14:29:59 +0100

    feat: added routes to controller functions

--- server/routes.ts
+++ server/routes.ts
@@ -1,9 +1,41 @@
 import express from 'express';
+import projectsCtrl from './controllers/projects';
+import bugsCtrl from './controllers/bugs';
+import commentsCtrl from './controllers/comments';
+import detailsCtrl from './controllers/details';
+
 const router = express.Router();
 
-router.get('/', (req: express.Request, res: express.Response) => {
-  res.status(200);
-  res.send('Hello World!');
-});
+//=============
+// PROJECTS
+//=============
+router.get('/projects', projectsCtrl.getProjects);
+router.post('/projects', projectsCtrl.createProject);
+
+//=============
+// USERS
+//=============
+
+// TODO create user or use OAUTH??
+
+//=============
+// BUGS
+//=============
+
+router.get('/bugs', bugsCtrl.getBugs);
+router.post('/bugs', bugsCtrl.createBug);
+router.patch('/bugs', bugsCtrl.editBug);
+
+//=============
+// COMMENTS
+//=============
+
+router.post('/comments', commentsCtrl.createComment);
+
+//=============
+// DETAILS
+//=============
+
+router.get('/details', detailsCtrl.getDetails); //TODO middleware that adds comments to response
 
 export default router;
