24aa42f24d6e5d0795d91f77bb686e40509c1be9
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-10-13 11:39:39 +0100

    chore: deleted boilerplate comments

--- server/db/models/comment.js
+++ server/db/models/comment.js
@@ -2,13 +2,7 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class comment extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       comment.belongsTo(models.bug, {
         foreignKey: {
           allowNull: false,
--- server/db/models/projectuser.js
+++ server/db/models/projectuser.js
@@ -2,14 +2,7 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class projectuser extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
-
       projectuser.hasMany(models.user, {
         foreignKey: 'id',
       });
