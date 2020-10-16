597c070a7ee9609259b702fe5276a2b1a1b81e33
Hamish <chan.hamish@gmail.com>
2020-10-16 18:03:11 +0100

    feat: click outside logout menu to close it

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import Logo from '../assets/zapp.png';
+import onClickOutside from 'react-onclickoutside';
 
 import Context from '../Context';
 
@@ -8,14 +9,22 @@ import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { SERVER } from '../constants';
 
-const Header = () => {
+type ElementFunction = () => JSX.Element;
+
+interface HeaderComponent extends ElementFunction {
+  handleClickOutside?: () => void;
+}
+
+const Header: HeaderComponent= () => {
   const ctx = useContext(Context);
   const [isOpen, setIsOpen] = useState(false);
 
+  Header.handleClickOutside = () => setIsOpen(false);
+
   const logout = () => {
-    ctx.dispatch({type: 'logout'});
+    ctx.dispatch({ type: 'logout' });
     window.open(`${SERVER}/auth/logout`, '_self');
-  }
+  };
 
   return (
     <div>
@@ -34,9 +43,7 @@ const Header = () => {
         )}
       </div>
       {isOpen && (
-        <div
-          className="absolute right-0 mr-2 h-12 z-10 shadow-md bg-white flex flex-col cursor-pointer"
-        >
+        <div className="absolute right-0 mr-2 h-12 z-10 shadow-md bg-white flex flex-col cursor-pointer">
           <div className="flex self-center hover:bg-gray-200" onClick={logout}>
             <FontAwesomeIcon icon={faSignOutAlt} size={'lg'} className="m-3" />
             <div className="self-center mr-4">Logout</div>
@@ -47,4 +54,8 @@ const Header = () => {
   );
 };
 
-export default Header;
+const clickOutsideConfig = {
+  handleClickOutside: () => Header.handleClickOutside,
+};
+
+export default onClickOutside(Header, clickOutsideConfig);
