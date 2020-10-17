b5d97fadfba4fd903aaf79f7b4410ee723865254
Hamish <chan.hamish@gmail.com>
2020-10-17 12:46:28 +0100

    feat: add useUsers hook

---
+++ client/src/hooks/useUsers.tsx
@@ -0,0 +1,12 @@
+import { useQuery, QueryResult } from 'react-query';
+import fetchRequest from '../services/ApiService';
+import { User } from '../types/User';
+
+// Get all users for one project
+const useBugs = (projectId: number): QueryResult<User[]> => {
+  return useQuery(projectId && ['projectbugs', projectId], () => {
+    return fetchRequest(`/projectsUsers?projectId=${projectId}`);
+  });
+};
+
+export default useBugs;
