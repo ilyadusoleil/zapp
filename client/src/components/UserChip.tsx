e4206a0e55d5235a4fd92975dada9ab3f609b650
Hamish <chan.hamish@gmail.com>
2020-10-17 13:40:08 +0100

    feat: user chip component

---
+++ client/src/components/UserChip.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGrinAlt } from '@fortawesome/free-solid-svg-icons';
+
+import useUser from '../hooks/useUser';
+
+const UserChip = ({userId}: {userId: number}) => {
+  if (!userId) userId = 0;
+  
+  const {isLoading, data} = useUser(userId)
+
+  if (userId == 0) return <div className="font-bold mx-2">Not Assigned</div>
+
+  if (isLoading || !data) return <div>unknown user</div>
+
+  return (
+    <div className="flex">
+          {data.image ? (
+      <img
+        src={data.image}
+        className="rounded-full h-6 mx-1"
+        alt="user avatar"
+      />
+    ) : (
+      <FontAwesomeIcon
+        icon={faGrinAlt}
+        size={'lg'}
+        className="mx-1"
+      />
+    )}
+    <div className="font-bold mx-2">{data.displayName || data.firstName}</div>
+    </div>
+
+  )
+}
+
+export default UserChip;
