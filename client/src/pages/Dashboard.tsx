3ff8199cef2d68f64f3938713ea29e61f367db9e
Hamish <chan.hamish@gmail.com>
2020-10-19 15:23:32 +0100

    feat: add filtering functionality to dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -17,30 +17,66 @@ interface DashboardProps extends RouteComponentProps {
 
 type selectInfo = {
   label: string;
-  sortFunction: (a: Bug, b: Bug) => number;
+  func: (a: Bug, b: Bug) => number;
 };
 
-const SELECT_INFO: selectInfo[] = [
-  { label: 'High-Low', sortFunction: (a, b) => a.priority - b.priority },
-  { label: 'Low-High', sortFunction: (a, b) => b.priority - a.priority },
+const sortPriority = (a: Bug, b: Bug) => a.priority - b.priority;
+const sortCreated = (a: Bug, b: Bug) =>
+  new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+
+const SORT_INFO: selectInfo[] = [
+  {
+    label: 'High-Low',
+    func: (a, b) => sortPriority(a, b) || sortCreated(a, b),
+  },
+  {
+    label: 'Low-High',
+    func: (a, b) => sortPriority(b, a) || sortCreated(a, b),
+  },
   {
     label: 'First-Last',
-    sortFunction: (a, b) =>
-      new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
+    func: (a, b) => sortCreated(a, b),
   },
   {
     label: 'Last-First',
-    sortFunction: (a, b) =>
-      new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
+    func: (a, b) => sortCreated(b, a),
   },
 ];
+
+type selectFilterInfo = {
+  label: string;
+  func: (a: Bug) => boolean;
+};
 
 const Dashboard = ({ id: projectId }: DashboardProps) => {
   if (!projectId) projectId = '0';
 
   const ctx = useContext(Context);
   const { isLoading, isError, data } = useBugs(parseInt(projectId));
+
   const [sortIdx, setSortIdx] = useState(0);
+  const [assigneeFilterIdx, setAssigneeFilterIdx] = useState(0);
+  const [isShowCompleted, setIsShowCompleted] = useState(false);
+
+  const ASSIGNEE_FILTER_INFO: selectFilterInfo[] = [
+    {
+      label: 'All',
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      func: (_a) => true,
+    },
+    {
+      label: 'Just me',
+      func: (a) => (a.userId ? a.userId == ctx.state.userId : false),
+    },
+    { label: 'Unassigned', func: (a) => !a.userId },
+  ];
+
+  const updateSortSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortIdx(parseInt(e.target.value));
+  }
+  const updateAssigneeFilterSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setAssigneeFilterIdx(parseInt(e.target.value));
+  }
 
   useEffect(() => {
     ctx.dispatch({ type: 'setCurrentProjectId', payload: projectId });
@@ -60,21 +96,45 @@ const Dashboard = ({ id: projectId }: DashboardProps) => {
 
       <div className="mx-16">
         <ProjectHeader projectId={parseInt(projectId)} />
-        <h1>Dashboard</h1>
+        <h1 className="mb-5">Dashboard</h1>
+
         <select
-          onBlur={(e) => {
-            setSortIdx(parseInt(e.target.value));
-          }}
+          className="mr-5"
+          onChange={updateSortSelect}
+          onBlur={updateSortSelect}
         >
-          {SELECT_INFO.map((el, i) => (
+          {SORT_INFO.map((el, i) => (
             <option value={i} key={i}>
               {el.label}
             </option>
           ))}
         </select>
 
+        <select
+          className="mr-5"
+          onChange={updateAssigneeFilterSelect}
+          onBlur={updateAssigneeFilterSelect}
+        >
+          {ASSIGNEE_FILTER_INFO.map((el, i) => (
+            <option value={i} key={i}>
+              {el.label}
+            </option>
+          ))}
+        </select>
+
+        <label>
+          Show Completed
+          <input
+            className="ml-3"
+            type="checkbox"
+            onChange={(e) => setIsShowCompleted(e.target.checked)}
+          />
+        </label>
+
         {[...data]
-          .sort(SELECT_INFO[sortIdx].sortFunction)
+          .filter((bug) => (isShowCompleted ? true : bug.state == 0))
+          .filter(ASSIGNEE_FILTER_INFO[assigneeFilterIdx].func)
+          .sort(SORT_INFO[sortIdx].func)
           .map((bug: Bug, index) => (
             <Bugitem key={index} bug={bug} />
           ))}
