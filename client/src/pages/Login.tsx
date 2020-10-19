85b21ae6d56459af46537776d8a0d1fced7e3109
Rupert Bright <rupert.bright@live.co.uk>
2020-10-19 10:05:07 +0100

    style: login-page design (reviewed changes)

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -7,10 +7,6 @@ import { SERVER } from '../constants';
 import GoogleButton from 'react-google-button';
 import Zapp from '../assets/zappcopy.png'
 
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBug, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
-
 const Login = (_props: RouteComponentProps) => { // eslint-disable-line @typescript-eslint/no-unused-vars
   const handleLogin = () => {
     window.open(`${SERVER}/auth/google`, '_self'); // TODO: change this when oauth implementation
@@ -21,7 +17,7 @@ const Login = (_props: RouteComponentProps) => { // eslint-disable-line @typescr
 <div>
     <div className='rounded-none bg-gradient-to-r from-indigo-800 to-purple-500 h-screen flex justify-center xl:rounded-tl-full xl:rounded-br-full'>
         <div className='mt-32'>
-                <img className='h-24 m-auto animate-pulse xl:mt-16 ' src={Zapp}></img>
+                <img className='h-24 m-auto animate-pulse xl:mt-16 ' alt='logo' src={Zapp}></img>
                 <h1 className='font-display text-6xl font-bold text-white  text-center mt-4'>Zapp</h1>
                 <h3 className='text-2xl text-gray-300 text-center p-4 font-body'>Lorem ipsum dolor sit amet, adhuc singulis sadipscing eu mei,</h3>
             <div className='mt-12'>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,27 +1,17 @@
 module.exports = {
-  future: {
+future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
-  },
-  purge: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.jsx'],
-  theme: {
-      fontFamily: {
-          'display': ['PT Sans'],
-          'body': ['PT Sans Narrow'],
-      },
-    extend: {
-        keyframes: {
-            wiggle: {
-            '0%, 100%': { transform: 'rotate(-3deg)' },
-            '50%': { transform: 'rotate(3deg)' },
-            }
-        },
-
-        animation: {
-            wiggle: 'wiggle 1s ease-in-out infinite',
-            },
+},
+purge: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.jsx'],
+theme: {
+    fontFamily: {
+        'display': ['PT Sans'],
+        'body': ['PT Sans Narrow'],
     },
-  },
-  variants: {},
-  plugins: [],
+    extend: {
+    },
+},
+variants: {},
+plugins: [],
 };
