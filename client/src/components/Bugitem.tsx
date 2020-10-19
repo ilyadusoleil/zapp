8725f48dc2a8ca80fc0e815ad78e6655e40f7ece
Rupert Bright <rupert.bright@live.co.uk>
2020-10-19 23:06:19 +0100

    style/new bug form, flex and header

--- client/src/components/Bugitem.tsx
+++ client/src/components/Bugitem.tsx
@@ -14,12 +14,14 @@ type Props = {
   bug: Bug;
 };
 
+
+
 function Bugitem({ bug }: Props) {
 
 
 return (
 
-    <div className="w-full mt-4 mb-4 p-1 m-1 bg-gray-100  border b-gray-200 rounded-lg shadow-lg lg:w-md-container  xl:w-container  ">
+    <div className='w-full flex-grow mt-4 mb-4 p-1 m-1 bg-gray-100  border border-gray-200 rounded-lg shadow-lg lg:w-md-container  xl:w-container'>
       <div
 
         role="button"
@@ -64,4 +66,5 @@ return (
 }
 
 export default Bugitem;
+
 
