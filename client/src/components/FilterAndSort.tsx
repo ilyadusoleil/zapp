f9e200dd3d46c62d6137a581143acc7a28d3e49e
Hamish <chan.hamish@gmail.com>
2020-10-20 11:50:52 +0100

    feat: filter and sort component

---
+++ client/src/components/FilterAndSort.tsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import { Bug } from '../types/Bug';
+
+import { Collapse } from 'react-collapse';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+
+type selectInfo = {
+  label: string;
+  func: (a: Bug, b: Bug) => number;
+};
+
+const sortPriority = (a: Bug, b: Bug) => a.priority - b.priority;
+const sortCreated = (a: Bug, b: Bug) =>
+  new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+
+export const SORT_INFO: selectInfo[] = [
+  {
+    label: 'High-Low',
+    func: (a, b) => sortPriority(a, b) || sortCreated(a, b),
+  },
+  {
+    label: 'Low-High',
+    func: (a, b) => sortPriority(b, a) || sortCreated(a, b),
+  },
+  {
+    label: 'First-Last',
+    func: (a, b) => sortCreated(a, b),
+  },
+  {
+    label: 'Last-First',
+    func: (a, b) => sortCreated(b, a),
+  },
+];
+
+type selectFilterInfo = {
+  label: string;
+  func: (a: Bug, userId?: number) => boolean;
+};
+
+export const ASSIGNEE_FILTER_INFO: selectFilterInfo[] = [
+  {
+    label: 'All',
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    func: (_a) => true,
+  },
+  {
+    label: 'Just me',
+    func: (a, userId) => (a.userId ? a.userId == userId : false),
+  },
+  { label: 'Unassigned', func: (a) => !a.userId },
+];
+
+type FilterAndSortType = {
+  sortIdx: number;
+  setSortIdx: React.Dispatch<React.SetStateAction<number>>;
+  assigneeFilterIdx: number;
+  setAssigneeFilterIdx: React.Dispatch<React.SetStateAction<number>>;
+  isShowCompleted: boolean;
+  setIsShowCompleted: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const FilterAndSort = ({
+  sortIdx,
+  setSortIdx,
+  assigneeFilterIdx,
+  setAssigneeFilterIdx,
+  isShowCompleted,
+  setIsShowCompleted,
+}: FilterAndSortType) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => setIsOpen(!isOpen);
+
+  const updateSortSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortIdx(parseInt(e.target.value));
+  };
+
+  const updateAssigneeFilterSelect = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setAssigneeFilterIdx(parseInt(e.target.value));
+  };
+
+  return (
+    <>
+      <div className="mt-3 m-2 cursor-pointer" onClick={toggleCollapse}>
+        <FontAwesomeIcon icon={isOpen ? faChevronUp : faChevronDown} className="mx-4"/>
+        Filter and Sort
+      </div>
+      <Collapse isOpened={isOpen}>
+        <div className="flex ml-4">
+          <select
+            className="mr-5"
+            onChange={updateSortSelect}
+            onBlur={updateSortSelect}
+          >
+            {SORT_INFO.map((el, i) => (
+              <option value={i} key={i}>
+                {el.label}
+              </option>
+            ))}
+          </select>
+
+          <select
+            className="mr-5"
+            onChange={updateAssigneeFilterSelect}
+            onBlur={updateAssigneeFilterSelect}
+          >
+            {ASSIGNEE_FILTER_INFO.map((el, i) => (
+              <option value={i} key={i}>
+                {el.label}
+              </option>
+            ))}
+          </select>
+
+          <label>
+            Show Completed
+            <input
+              className="ml-3"
+              type="checkbox"
+              onChange={(e) => setIsShowCompleted(e.target.checked)}
+            />
+          </label>
+        </div>
+      </Collapse>
+    </>
+  );
+};
+
+export default FilterAndSort;
