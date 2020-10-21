1783b3518ea54cbedb44eca2fea48ca11559b372
Rupert Bright <rupert.bright@live.co.uk>
2020-10-21 13:11:16 +0100

    chore: fix-issues

--- client/src/components/Bugitem.tsx
+++ client/src/components/Bugitem.tsx
@@ -17,28 +17,31 @@ type Props = {
 
 const bugCategoryIcons = [faBug, faLightbulb];
 
-
-
 function Bugitem({ bug }: Props) {
+  const topBorderStyle = () => {
+    return bug.state === 0
+      ? 'bg-gradient-to-r from-teal-500  to-teal-400'
+      : 'bg-gradient-to-r from-gray-500  to-gray-400';
+  };
 
-    function topBar () {
-        return (bug.state === 0) ? 'bg-gradient-to-r from-teal-500  to-teal-400' : 'bg-gradient-to-r from-gray-500  to-gray-400'
-    }
+  const bugItemComplete = () => {
+    return bug.state !== 0 ? 'opacity-50' : '';
+  };
 
-    function bugItemComplete () {
-        return (bug.state !== 0) ? 'opacity-50' : '';
-    }
-
-   const goToBugDetails = () => {
-        return navigate(`/details/${bug.id}`)
-    }
+  const goToBugDetails = () => {
+    return navigate(`/details/${bug.id}`);
+  };
 
   return (
-    <div onClick={goToBugDetails} onKeyDown={goToBugDetails} role="button" tabIndex={0} className={`w-full flex-grow ${bugItemComplete()} cursor-pointer mt-4 mb-4 m-1 ml-3 bg-gray-100 shadow-lg hover:shadow-2xl hover:cursor-pointer md:w-md-container md:flex-grow-0 xl:w-container`}>
-      <div className={`${topBar()} h-1`}></div>
-      <div
-        className="p-1 flex justify-items"
-      >
+    <div
+      onClick={goToBugDetails}
+      onKeyDown={goToBugDetails}
+      role="button"
+      tabIndex={0}
+      className={`w-full flex-grow ${bugItemComplete()} cursor-pointer mt-4 mb-4 m-1 ml-3 bg-gray-100 shadow-lg hover:shadow-2xl hover:cursor-pointer md:w-md-container md:flex-grow-0 xl:w-container`}
+    >
+      <div className={`${topBorderStyle()} h-1`}></div>
+      <div className="p-1 flex justify-items">
         <div className="flex items-center justify-center mt-1 ml-2 bg-gradient-to-r from-indigo-800 to-purple-500 rounded-full w-8 h-8">
           <FontAwesomeIcon
             className="text-center text-gray-100"
@@ -61,12 +64,14 @@ function Bugitem({ bug }: Props) {
         </div>
         <div className="">
           <h1 className="text-base font-bold font-display mt-4">Description</h1>
-          <p className="py-2 tracking-wider font-body h-16 overflow-y-auto">{bug.description}</p>
+          <p className="py-2 tracking-wider font-body h-16 overflow-y-auto">
+            {bug.description}
+          </p>
         </div>
       </div>
       <div className="flex p-1 mb-1">
         <div className="ml-auto p-1 bg-gradient-to-r from-gray-200  to-gray-300 rounded-full">
-            <UserChip userId={bug.userId} />
+          <UserChip userId={bug.userId} />
         </div>
       </div>
     </div>
